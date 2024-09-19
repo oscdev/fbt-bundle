9e575eaad6770cb94ee799ed50c3b1afa5dad1dc
ShraddhaR <shraddhar@oscprofessionals.in>
2024-09-19 12:15:48 +0530

    update readme file and git ignore folder

---
+++ cnf.js
@@ -0,0 +1,12 @@
+export const cnf = {
+    dev: {
+        HOST: "localhost",
+        USER: "root",
+        PASSWORD: "1688*RoLon!",
+        DB: "upsell_live"
+    },
+    debug: true,
+    UUID: "dee3dde5-6a4a-4123-8f24-9010aafd0c8a",
+    APPNAME: "oscp-upsell-cross-sell-1",
+    SITEURL: "https://www.oscprofessionals.com"   
+};
