4eb58a83dc607f9a609f4fd9c135c9777bd9c8d2
prafulG <praful.gupta.21@gmail.com>
2024-09-20 15:30:58 +0530

    init

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -25,7 +25,7 @@ export default function App() {
         <Link to="/app" rel="home">
           Home
         </Link>
-        <Link to="/app/additional">Additional page</Link>
+        <Link to="/app/bundle/new">Bundle</Link>
       </NavMenu>
       <Outlet />
     </AppProvider>
