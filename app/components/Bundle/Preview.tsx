ddcc26d5ffef9445acfd5dd2bbae87f1b5a0305e
prafulG <praful.gupta.21@gmail.com>
2024-09-24 21:22:38 +0530

    manage bundle list

--- app/components/Bundle/Preview.tsx
+++ app/components/Bundle/Preview.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { LegacyCard, Text, BlockStack, InlineStack, SkeletonThumbnail, SkeletonBodyText, Card, Button, Thumbnail } from '@shopify/polaris'
 
 export function Preview(pros) {
-  const { bundleName, description, cartItemsMedia } = pros;
+  const { bundleName, description, cartItems, cartItemsMedia } = pros;
   return (
     <LegacyCard>
       <LegacyCard.Section>
@@ -12,20 +12,31 @@ export function Preview(pros) {
             <BlockStack gap="300">
               <Text alignment="center" variant="headingMd" as="h6">{(bundleName.value) ? bundleName.value : "Bundle of 3 Combos"}</Text>
               <Text variant="bodyLg" as="p">{(description.value) ? description.value : "Buy this combo and save 10% OFF"}</Text>
-              {cartItemsMedia.map(
-                ({ node: { title, featuredImage } }, index) => (
+
+              {cartItems.map(
+                ({ merchandiseId }, index) => (
                   <>
-                    <InlineStack align="start" wrap={false} gap="300">
-                      <Thumbnail
-                        source={featuredImage.url}
-                        alt={title}
-                      />
-                      {title}
-                    </InlineStack>
-                    {(index !== cartItemsMedia.length - 1) ? <Text variant="bodyMd" alignment='center' as="h3"> ------------ + ------------</Text> : ''}
+                    {cartItemsMedia.map(
+                      ({ node: { id, title, featuredImage } }) => (
+                        (merchandiseId.value == id.split("/").pop()) && (
+                          <>
+                            <InlineStack align="start" wrap={false} gap="300">
+                              <Thumbnail
+                                source={featuredImage.url}
+                                alt={title}
+                              />
+                              {title}
+                            </InlineStack>
+                            {(index !== cartItemsMedia.length - 1) ? <Text variant="bodyMd" alignment='center' as="h3"> ------------ + ------------</Text> : ''}
+                          </>
+                        )
+                      ),
+                    )}
                   </>
                 ),
               )}
+
+
 
               {(!cartItemsMedia.length) ? <>
                 <InlineStack align="start" wrap={false} gap="300">
@@ -41,14 +52,14 @@ export function Preview(pros) {
                 <InlineStack align="start" wrap={false} gap="300">
                   <SkeletonThumbnail size="small" />
                   <SkeletonBodyText lines={2} />
-                </InlineStack>                
+                </InlineStack>
               </> : null}
 
               <InlineStack align="start" wrap={false} gap="300">
-                  <Text variant="bodyMd" as="p">Total: </Text>
-                  <Text variant="bodyMd" as="p">Rs.00.00</Text>
-                  <Text variant="bodyMd" as="p" textDecorationLine="line-through">Rs.00.00</Text>
-                </InlineStack>
+                <Text variant="bodyMd" as="p">Total: </Text>
+                <Text variant="bodyMd" as="p">Rs.00.00</Text>
+                <Text variant="bodyMd" as="p" textDecorationLine="line-through">Rs.00.00</Text>
+              </InlineStack>
 
               <Button disabled>Add to cart | Save 10%    </Button>
             </BlockStack>
