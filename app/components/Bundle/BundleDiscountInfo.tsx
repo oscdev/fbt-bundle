d8c8b996b184a01afbd90512ab83dc5500ba2d2f
prafulG <praful.gupta.21@gmail.com>
2024-10-08 21:11:38 +0530

    Discounts Active date

--- app/components/Bundle/BundleDiscountInfo.tsx
+++ app/components/Bundle/BundleDiscountInfo.tsx
@@ -1,5 +1,5 @@
 import { useState, useCallback } from "react";
-import { TextField, Card, Form, FormLayout, Select, Text, BlockStack, InlineStack, Checkbox, Popover, DatePicker, Button, PageActions } from "@shopify/polaris";
+import { TextField, Card, Form, FormLayout, Select, Text, BlockStack, InlineStack, Checkbox, Popover, DatePicker, Button, PageActions, IndexTable, Box } from "@shopify/polaris";
 import { XIcon, ArrowDownIcon, ArrowUpIcon } from '@shopify/polaris-icons';
 export function BundleDiscountInfo(pros) {
   const { globalPriceRules, onAddGlobalPriceRules, onRemoveGlobalPriceRules, bundlePrice, cartItems, cartItemsMedia, currencyCodes, calculatePrice, onCalculatePrice } = pros;
@@ -8,6 +8,7 @@ export function BundleDiscountInfo(pros) {
   const [startVisible, setStartVisible] = useState(false);
   const [endVisible, setEndVisible] = useState(false);
   const [activeRowIndex, setActiveRowIndex] = useState();
+  const [endDateActiveRowIndex, setEndDateActiveRowIndex] = useState();
 
   function getFormatedDate(date) {
     if (!date) return;
@@ -17,6 +18,125 @@ export function BundleDiscountInfo(pros) {
     const d = new Date(date);
     return monthNames[d.getMonth()] + ' ' + d.getDate() + ', ' + d.getFullYear();
   }
+
+  const discoubtsRowMarkup = globalPriceRules.map(({ type, value, startAt, endAt }, index) => (
+    <IndexTable.Row
+      id={index}
+      key={index}
+      position={index}
+    >
+      <IndexTable.Cell>
+        <TextField
+          placeholder="E.g. 10"
+          prefix="%"
+          type="number"
+          max={99.99}
+          min={0.01}
+          label="Discount Value"
+          labelHidden
+          value={value.value}
+          onChange={(e) => {
+            if ((parseFloat(e) < 100) || (e == '')) value.onChange(e)
+          }
+          }
+          autoComplete="off"
+        />
+      </IndexTable.Cell>
+      <IndexTable.Cell>
+        <Popover
+          active={startVisible && activeRowIndex === index}
+          activator={<TextField
+            label="Start At"
+            labelHidden
+            value={getFormatedDate(startAt.value)}
+            autoComplete="off"
+            onFocus={() => {
+              setActiveRowIndex(index);
+              setStartVisible(true);
+            }}
+          />}
+          autofocusTarget="first-node"
+          onClose={() => setStartVisible(false)}
+        >
+          <Card>
+            <DatePicker
+              month={new Date(startAt.value).getMonth()}
+              year={new Date(startAt.value).getFullYear()}
+              disableDatesBefore={new Date(Date.now() - 1000 * 60 * 60 * 24)}
+              onChange={(date) => {
+                startAt.onChange(new Date(date.start.getTime() + (1000 * 60 * 60 * 24)).toISOString().split('T')[0]);
+                setStartVisible(false);
+              }}
+              selected={new Date(startAt.value)}
+            />
+          </Card>
+        </Popover>
+      </IndexTable.Cell>
+      <IndexTable.Cell>
+        <InlineStack align="start" wrap={false} gap="300">
+          <Box>
+            <Checkbox
+              label="Basic checkbox"
+              labelHidden={true}
+              checked={endAt.value ? true : false}
+              onChange={(e) => {
+                console.log(e);
+                if (e && !endAt.value) {
+                  endAt.onChange(new Date(new Date(startAt.value).getTime() + (1000 * 60 * 60 * 24)))
+                  //setActiveRowIndex(index);
+                } else {
+                  endAt.onChange(null);
+                  setEndDateEnable(e);
+                }
+              }}
+              onFocus={() => setEndVisible(true)}
+            />
+          </Box>
+          <Box>
+            <Popover
+              active={(endDateEnable) && endVisible && endDateActiveRowIndex === index}
+              activator={<TextField
+                label="End At"
+                labelHidden
+                value={getFormatedDate(endAt.value)}
+                autoComplete="off"
+                placeholder="End At"
+                disabled={!endAt.value ? true : false}
+                onFocus={() => {
+                  setEndVisible(true);
+                  setEndDateEnable(true);
+                  setEndDateActiveRowIndex(index);
+                }}
+              />}
+              autofocusTarget="first-node"
+              onClose={() => setEndVisible(false)}
+              preferredPosition="below"
+              ariaHaspopup={false}
+            >
+              <Card>
+                <DatePicker
+                  month={(endAt.value) ? new Date(endAt.value).getMonth() : new Date().getMonth()}
+                  year={(endAt.value) ? new Date(endAt.value).getFullYear() : new Date().getFullYear()}
+                  disableDatesBefore={new Date(startAt.value)}
+                  onChange={(date) => {
+                    endAt.onChange(new Date(date.end.getTime() + (1000 * 60 * 60 * 24)).toISOString().split('T')[0]);
+                    setEndVisible(false);
+                  }}
+                  selected={(endAt.value) ? new Date(endAt.value) : new Date(new Date(startAt.value).getTime() + (1000 * 60 * 60 * 24))}
+                />
+              </Card>
+            </Popover>
+          </Box>
+        </InlineStack>
+      </IndexTable.Cell>
+      <IndexTable.Cell>
+        <Button size="large" variant="plain" tone="critical" icon={XIcon} onClick={() => {
+          onRemoveGlobalPriceRules(index)
+        }}></Button>
+      </IndexTable.Cell>
+    </IndexTable.Row>
+  ),
+  );
 
   return (
     <Card>
@@ -58,131 +178,41 @@ export function BundleDiscountInfo(pros) {
                 }}
               />
             </BlockStack>
-            {globalPriceRules.map(({ type, value, startAt, endAt }, index) => (
-              <>
-                <BlockStack gap="300" key={"price-" + index}>
-                  <InlineStack align="start" wrap={false} gap="300">
-                    <TextField
-                      placeholder="E.g. 10"
-                      prefix="%"
-                      type="number"
-                      max={99.99}
-                      min={0.01}
-                      label="Discount Value"
-                      value={value.value}
-                      onChange={(e) => {
-                        if ((parseFloat(e) < 100) || (e == '')) value.onChange(e)
-                        //value.onChange(e)
-                      }
-                      }
-                      autoComplete="off"
-                    />
-                    <Popover
-                      active={startVisible && activeRowIndex === index}
-                      activator={<TextField
-                        label="Start At"
-                        value={getFormatedDate(startAt.value)}
-                        autoComplete="off"
-                        onFocus={() => {
-                          setActiveRowIndex(index);
-                          setStartVisible(true);
-                        }}
-                      />}
-                      autofocusTarget="first-node"
-                      onClose={() => setStartVisible(false)}
-                    >
-                      <Card>
-                        <DatePicker
-                          month={new Date(startAt.value).getMonth()}
-                          year={new Date(startAt.value).getFullYear()}
-                          disableDatesBefore={new Date(Date.now() - 1000 * 60 * 60 * 24)}
-                          onChange={(date) => {
-                            startAt.onChange(new Date(date.start.getTime() + (1000 * 60 * 60 * 24)).toISOString().split('T')[0]);
-                            setStartVisible(false);
-                          }}
-                          selected={new Date(startAt.value)}
-                        />
-                      </Card>
-
-                    </Popover>
-                    <Popover
-                      active={(endDateEnable) && endVisible && activeRowIndex === index}
-                      activator={<TextField
-                        label="End At"
-                        value={getFormatedDate(endAt.value)}
-                        autoComplete="off"
-                        placeholder="End At"
-                        disabled={!endAt.value ? true : false}
-                        onFocus={() => setEndVisible(true)}
-                        connectedLeft={<Checkbox
-                          label="Basic checkbox"
-                          labelHidden={true}
-                          //checked={endDateEnable}
-                          checked={endAt.value ? true : false}
-                          onChange={(e) => {
-                            console.log(e);
-                            if(e && !endAt.value) {
-                              endAt.onChange(new Date(new Date(startAt.value).getTime() + (1000 * 60 * 60 * 24)))
-                              setActiveRowIndex(index);                              
-                            }else{
-                              endAt.onChange(null)
-                            }
-                            setEndDateEnable(e);
-
-                            // setEndDateEnable(e);
-                            // setActiveRowIndex(index);
-                            // setTimeout(() => {
-                            //   (endDateEnable) && endAt.onChange(null)
-                            // }, 200);
-                          }}
-                          onFocus={() => setEndVisible(true)}
-                        />}
-                      />}
-                      autofocusTarget="first-node"
-                      onClose={() => setEndVisible(false)}
-                    >
-                      <Card>
-                        <DatePicker
-                          month={(endAt.value) ? new Date(endAt.value).getMonth() : new Date().getMonth()}
-                          year={(endAt.value) ? new Date(endAt.value).getFullYear() : new Date().getFullYear()}
-                          disableDatesBefore={new Date(startAt.value)}
-                          onChange={(date) => {
-                            endAt.onChange(new Date(date.end.getTime() + (1000 * 60 * 60 * 24)).toISOString().split('T')[0]);
-                            setEndVisible(false);
-                          }}
-                          selected={(endAt.value) ? new Date(endAt.value) : new Date(new Date(startAt.value).getTime() + (1000 * 60 * 60 * 24))}
-                        />
-                      </Card>
-
-                    </Popover>
-                    <Button size="large" variant="plain" tone="critical" icon={XIcon} onClick={() => { 
-                      onRemoveGlobalPriceRules(index)
-                    }}></Button>
-                  </InlineStack>
-                </BlockStack>
-              </>
-            )
-            )}
-
             {((globalPriceRules.length)) && (
-              <PageActions
-                primaryAction={{
-                  content: 'More Discounts',
-                  disabled: (globalPriceRules.length >= 3) ? true : false,
-                  onAction: () => {
-                    const today = new Date();
-                    onAddGlobalPriceRules({
-                      value: '',
-                      type: 'percent',
-                      startAt: today.toISOString().split('T')[0],
-                      endAt: null
-                    })
-                  }
-                }}
-              />
+              <>
+                <IndexTable
+                  resourceName={{
+                    singular: 'Discount',
+                    plural: 'Discounts',
+                  }}
+                  itemCount={globalPriceRules.length}
+                  headings={[
+                    { title: 'Discount Value' },
+                    { title: 'Start At' },
+                    { title: 'End At' },
+                    { title: '' }
+                  ]}
+                  selectable={false}
+                >
+                  {discoubtsRowMarkup}
+                </IndexTable>
+                <PageActions
+                  primaryAction={{
+                    content: 'More Discounts',
+                    disabled: (globalPriceRules.length >= 3) ? true : false,
+                    onAction: () => {
+                      const today = new Date();
+                      onAddGlobalPriceRules({
+                        value: '',
+                        type: 'percent',
+                        startAt: today.toISOString().split('T')[0],
+                        endAt: null
+                      })
+                    }
+                  }}
+                />
+              </>
             )}
-
-
           </FormLayout>
         </Form>
       </BlockStack>
