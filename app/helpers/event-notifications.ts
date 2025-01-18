23f6edb50b455532486c528cddd1b2681bf20ba4
Guddi1387 <guddi.oscp@gmail.com>
2025-01-18 11:03:34 +0530

    app/helpers/event-notifications.ts

--- app/helpers/event-notifications.ts
+++ app/helpers/event-notifications.ts
@@ -27,8 +27,8 @@ events.on('oscp:send:owner:appInstalled', async (storeDetails) => {
         const mailOptions = {
             //to: 'guddi.oscp@gmail.com',
             to: email,
-            subject: "Welcome to the OSCP Upsell & Cross Sell App – We're Here to Assist You!",
-            html: "<p>Hello!</p><p>We’re thrilled to welcome you to the OSCP Upsell & Cross Sell App! You’ve made a great choice by installing our app, and we’re committed to making sure it helps your business grow.</p><p>To ensure everything runs smoothly, our team offers free setup assistance and is always available to answer any questions you might have. Whether you need help with configuration or have feature requests, we're here to make the app work perfectly for your store.</p><p>We’re also interested in learning more about how we can best serve your business. Do you have any specific requirements or features in mind? Let us know, and we’ll be happy to explore customized solutions that fit your needs.</p><p>Feel free to reach out to us anytime. We're just a message away!</p><p>Looking forward to working with you.</p><p>Best regards,<br>OSCP Upsell & Cross Sell App Team</p>",
+            subject: "Welcome to the OSCP Bundle & Upsell App – We're Here to Assist You!",
+            html: "<p>Hello!</p><p>We’re thrilled to welcome you to the OSCP Bundle & Upsell App! You’ve made a great choice by installing our app, and we’re committed to making sure it helps your business grow.</p><p>To ensure everything runs smoothly, our team offers free setup assistance and is always available to answer any questions you might have. Whether you need help with configuration or have feature requests, we're here to make the app work perfectly for your store.</p><p>We’re also interested in learning more about how we can best serve your business. Do you have any specific requirements or features in mind? Let us know, and we’ll be happy to explore customized solutions that fit your needs.</p><p>Feel free to reach out to us anytime. We're just a message away!</p><p>Looking forward to working with you.</p><p>Best regards,<br>OSCP Upsell & Cross Sell App Team</p>",
 
         };
 
@@ -52,8 +52,8 @@ events.on('oscp:send:owner:appUninstalled', async (storeDetails) => {
         const mailOptions = {
             //to: 'guddi.oscp@gmail.com',
             to: email,
-            subject: "OSCP Upsell & Cross Sell App Uninstalled from Your Store",
-            html: "<p>Hello!</p><p>We noticed that you recently uninstalled the OSCP Upsell & Cross Sell App from your Shopify store, and we’d like to ensure that everything is running smoothly on your end. If you experienced any issues or have questions regarding the app’s integration with your store, our team of experts is here to help.</p><p>If you have any specific feature requests or feedback, we’d love to hear from you. Don’t hesitate to reach out – we’re always just a message away!<p>Best regards,<br>OSCP Upsell & Cross Sell App Team</p>",
+            subject: "OSCP Bundle & Upsell App Uninstalled from Your Store",
+            html: "<p>Hello!</p><p>We noticed that you recently uninstalled the OSCP Bundle & Upsell App from your Shopify store, and we’d like to ensure that everything is running smoothly on your end. If you experienced any issues or have questions regarding the app’s integration with your store, our team of experts is here to help.</p><p>If you have any specific feature requests or feedback, we’d love to hear from you. Don’t hesitate to reach out – we’re always just a message away!<p>Best regards,<br>OSCP Bundle & Upsell App Team</p>",
 
         };
         transporter.sendMail(mailOptions, (error, info) => {
